c2d2e8bdc5e90f19553951594bdc5fd31662002e
developer <anavitoria_gouvea@usp.br>
2021-10-25 13:07:38 -0300

    database configured

--- source/backEnd/index.js
+++ source/backEnd/index.js
@@ -2,12 +2,21 @@ const chalk = require('chalk');
 const fastify = require('fastify')({
     logger: true
 })
-  
-fastify.post('/login', function (request, reply) {
-    reply.send({ hello: 'world' })
+const proConfig = require('./dataBase/db');
+const Pool = require("pg").Pool;
+const pool = new Pool(proConfig);
+
+fastify.get('/login', async function (request, reply) {
+    try {
+        const response = await pool.query('SELECT * FROM accounts');
+        reply.status(200).send(response.rows);
+    } catch (err) {
+        throw new Error(err);
+    }
+
+
 })
   
-  // Run the server!
 fastify.listen(3000, function (err, address) {
     if (err) {
       fastify.log.error(err)
