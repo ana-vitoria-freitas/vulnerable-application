10cc3a76a6480650226768498d3585f91e20112d
Ana Vitória Freitas <69052949+ana-vitoria-freitas@users.noreply.github.com>
2021-10-25 13:13:21 -0300

    Revert "database configured"

--- source/backEnd/index.js
+++ source/backEnd/index.js
@@ -2,21 +2,12 @@ const chalk = require('chalk');
 const fastify = require('fastify')({
     logger: true
 })
-const proConfig = require('./dataBase/db');
-const Pool = require("pg").Pool;
-const pool = new Pool(proConfig);
-
-fastify.get('/login', async function (request, reply) {
-    try {
-        const response = await pool.query('SELECT * FROM accounts');
-        reply.status(200).send(response.rows);
-    } catch (err) {
-        throw new Error(err);
-    }
-
-
+  
+fastify.post('/login', function (request, reply) {
+    reply.send({ hello: 'world' })
 })
   
+  // Run the server!
 fastify.listen(3000, function (err, address) {
     if (err) {
       fastify.log.error(err)
